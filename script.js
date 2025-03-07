9dff2e9746b17eeae9dcf26870f7f3b2c7fceb45
c41-gg <92499689+c41-gg@users.noreply.github.com>
2025-03-08 01:12:59 +0800

    Add Local Storage Saving

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const toDoList = document.getElementById('toDoList');
 
 let editToDo = null;
 
+//Add Function
 const addToDo = ()=> {
     const inputText = inputBox.value.trim();
     if(inputText.length <= 0){
         alert("Input a Task First");
     }else{
         if(addBtn.value=== "Edit"){
+            editLocalToDos(editToDo.target.previousElementSibling.innerHTML);
             editToDo.target.previousElementSibling.innerHTML = inputText;
             addBtn.value = "Add";
             inputBox.value = "";
@@ -33,17 +35,25 @@ const addToDo = ()=> {
             deleteBtn.innerText = "Remove";
             deleteBtn.classList.add("btn", "deleteBtn");
             li.appendChild(deleteBtn);
+            
+            
 
             toDoList.appendChild(li);
             inputBox.value="";
+
+            saveLocalToDos(inputText);
+
         }
     }  
 
 }
 
+//Edit Task Function
 const updateToDo = (e) => {
     if(e.target.innerHTML === "Remove"){
         toDoList.removeChild(e.target.parentElement);
+        deleteLocalTodos(e.target.parentElement);
+
     }
     if(e.target.innerHTML =="Edit"){
         inputBox.value= e.target.previousElementSibling.innerHTML;
@@ -54,6 +64,70 @@ const updateToDo = (e) => {
 }
 
 
+const saveLocalToDos=(toDo)=>{
+    let toDos;
+    if(localStorage.getItem("toDos") === null){
+        toDos = [];
+    }else{
+        toDos = JSON.parse(localStorage.getItem("toDos"));
+    }  
+    toDos.push(toDo);
+    localStorage.setItem("toDos", JSON.stringify(toDos))
+    console.log(localStorage.getItem("toDos"))
+    console.log(JSON.parse(localStorage.getItem("toDos")));
+}
 
+const getLocalToDos  = () => {
+    let toDos;
+    if(localStorage.getItem("toDos") === null){
+        toDos = [];
+    }else{
+        toDos = JSON.parse(localStorage.getItem("toDos"));
+        toDos.forEach(toDo => {
+            const li= document.createElement("li");
+            const p = document.createElement("p");
+            p.innerHTML= toDo;
+            li.appendChild(p);
+
+            //Edit Button
+            const editBtn = document.createElement("button");
+            editBtn.innerText = "Edit";
+            editBtn.classList.add("btn", "editBtn");
+            li.appendChild(editBtn);
+
+            
+            //Delete button
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerText = "Remove";
+            deleteBtn.classList.add("btn", "deleteBtn");
+            li.appendChild(deleteBtn);          
+
+            toDoList.appendChild(li);
+        })
+    }
+}
+
+const deleteLocalTodos = (toDo) => {
+    let toDos;
+    if(localStorage.getItem("toDos") === null){
+        toDos = [];
+    }else{
+        toDos = JSON.parse(localStorage.getItem("toDos"));
+    }  
+    let toDoText = toDo.children[0].innerHTML
+    let toDoIndex = toDos.indexOf(toDoText)
+    toDos.splice(toDoIndex, 1)
+    localStorage.setItem("toDos", JSON.stringify(toDos))
+    console.log(toDoIndex);
+}
+
+const editLocalToDos = (toDo) =>{
+    let toDos = JSON.parse(localStorage.getItem("toDos"));
+    let toDoIndex = toDos.indexOf(toDo);
+    toDos[toDoIndex] = inputBox.value;
+    localStorage.setItem("toDos", JSON.stringify(toDos)); 
+}
+
+document.addEventListener('DOMContentLoaded', getLocalToDos)
 addBtn.addEventListener('click',addToDo);
 toDoList.addEventListener('click',updateToDo);
